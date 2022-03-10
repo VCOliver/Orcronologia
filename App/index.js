71fb01fe079ccc12d15530c17a7073346acaa11b
VCOliver <victorcruz.01@gmail.com>
2022-03-10 11:14:23 -0300

    Timer added

--- App/index.js
+++ App/index.js
@@ -1,5 +1,13 @@
 function addText(text){
-    var div = document.getElementById("content")
-    div.innerHTML = "<br>" + text
+    var div = document.getElementById("Time")
+    div.innerHTML = text
 }
 eel.expose(addText)
+
+function StopWatch(){
+    eel.StopWatch()
+}
+
+function haltWatch(){
+    eel.stop()
+}
--- main.py
+++ main.py
@@ -2,21 +2,23 @@ import eel
 import os
 from pathlib import Path
 
-PATH = Path(f'{os.path.dirname(os.path.realpath(__file__))}\App').as_posix()
+if os.name == 'nt':
+    PATH = Path(f'{os.path.dirname(os.path.realpath(__file__))}\App').as_posix()
+else:
+    PATH = 'App'
 
 @eel.expose
-def hello():
-    print("Hello world!")
+def stop():
+    global stopwatch
+    stopwatch = False
 
-if __name__ == "__main__":
-    eel.init(PATH)
-    eel.start("index.html", size=(1000,600), block=False) # This states that all code below this line will be read
-
+@eel.expose
+def StopWatch():
     hour = min = sec = 0
 
-    while True:
+    while stopwatch == True:
         time_elapsed = f"{hour:02d}:{min:02d}:{sec:02d}"
-        eel.addText(f"Time elapsed: {time_elapsed}")
+        eel.addText(time_elapsed)
         eel.sleep(1)
         if(59 > sec >= 0):
             sec = sec + 1
@@ -27,3 +29,11 @@ if __name__ == "__main__":
             sec = 0
             min = 0
             hour = hour + 1
+
+
+if __name__ == "__main__":
+
+    stopwatch = True
+
+    eel.init(PATH)
+    eel.start("index.html", size=(500,350), port=8080) # This states that all code below this line will be read
